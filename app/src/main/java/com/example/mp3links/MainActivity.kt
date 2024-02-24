3344fb150171b04ebec5e2176ccced012b8140de
hungnguyen <nguyenthanhhung.dev@gmail.com>
2024-02-24 14:20:45 +0700

    Update Giao Diện

--- app/src/main/java/com/example/mp3links/MainActivity.kt
+++ app/src/main/java/com/example/mp3links/MainActivity.kt
@@ -1,33 +1,23 @@
 package com.example.mp3links
 
-import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -38,15 +28,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
 import com.example.mp3links.ui.theme.MP3LinksTheme
@@ -71,8 +57,6 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MP3LinksScreen() {
-    var link by remember { mutableStateOf(TextFieldValue()) }
-
     Scaffold(
         topBar = {
             TopAppBar(
@@ -92,12 +76,47 @@ fun MP3LinksScreen() {
                 horizontalAlignment = Alignment.CenterHorizontally,
 //                verticalArrangement = Arrangement.Center
             ) {
-                OutlinedTextField(
-                    value = link,
-                    onValueChange = { link = it },
-                    label = { Text("Enter MP3 link") },
-                    modifier = Modifier.fillMaxWidth()
-                )
+//                OutlinedTextField(
+//                    value = link,
+//                    onValueChange = { link = it },
+//                    label = { Text("Enter MP3 link") },
+//                    modifier = Modifier.fillMaxWidth()
+//                )
+                val options = listOf("Option 1", "Option 2", "Option 3", "Option 4", "Option 5")
+                var isExpanded by remember { mutableStateOf(false) }
+                var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+                Box (
+
+                ){
+                    ExposedDropdownMenuBox(expanded = isExpanded, onExpandedChange = {
+                        newValue ->
+                            isExpanded = newValue
+                    }) {
+                        TextField(value = selectedOptionText,
+                            onValueChange = {},
+                            readOnly = true,
+                            label = { Text(text = "Album") },
+                            trailingIcon = {
+                                ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                            },
+                            colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                            modifier = Modifier.menuAnchor()
+                        )
+                        ExposedDropdownMenu(expanded = isExpanded, onDismissRequest = { isExpanded = false }) {
+                            options.forEach() {
+                                    selectedOption ->
+                                DropdownMenuItem(onClick = {
+                                    selectedOptionText = selectedOption
+                                    isExpanded = false
+
+                                },
+                                    text = { Text(text = selectedOption) }
+                                )
+                            }
+                        }
+                    }
+                }
                 Spacer(modifier = Modifier.height(16.dp))
                 Button(
                     onClick = { /* Tải MP3 từ liên kết */ },
