0bdc6907b60c1bab64bd88e4b00e72fccfed05c9
hungnguyen <nguyenthanhhung.dev@gmail.com>
2024-02-24 21:00:01 +0700

    Thêm Google API

---
+++ app/src/main/java/com/example/mp3links/Helper.kt
@@ -0,0 +1,19 @@
+package com.example.mp3links
+
+import android.content.Context
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class Helper {
+    companion object{
+        fun getGoogleSignInClient(context: Context): GoogleSignInClient {
+            val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+//                .requestScopes(Scope(DriveScopes.DRIVE_FILE), Scope(DriveScopes.DRIVE))
+                .build()
+
+            return GoogleSignIn.getClient(context, signInOptions)
+        }
+    }
+}
