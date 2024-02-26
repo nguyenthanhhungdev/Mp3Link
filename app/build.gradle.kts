63077404b960eb657e2f185cae0b5a8c6d20bd3f
hungnguyen <nguyenthanhhung.dev@gmail.com>
2024-02-26 13:28:59 +0700

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,8 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
